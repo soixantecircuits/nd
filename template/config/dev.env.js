adc8cd33afa633c6e63f39233b1a32d54130996b
hugohil <hugo@soixantecircuits.fr>
2016-07-17 20:23:02 +0200

    Add electron packaging  📦
    Template only

--- template/config/dev.env.js
+++ template/config/dev.env.js
@@ -1,6 +1,16 @@
-var merge = require('webpack-merge')
+{{#if electron}}
+// Do not use `webpack-merge` since there is no devDependencies in packaged app
 var prodEnv = require('./prod.env')
+{{else}}
+var merge = require('webpack-merge')
+{{/if}}
 
+{{#if electron}}
+module.exports = Object.assign({
+  NODE_ENV: '"development"'
+}, prodEnv)
+{{else}}
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"'
 })
+{{/if}}
--- template/config/index.js
+++ template/config/index.js
@@ -1,5 +1,6 @@
 // see http://vuejs-templates.github.io/webpack for documentation.
 var path = require('path')
+var package = require('../package.json')
 
 module.exports = {
   build: {
@@ -9,6 +10,31 @@ module.exports = {
     assetsSubDirectory: 'static',
     {{#if electron}}
     assetsPublicPath: path.resolve(__dirname, '../dist'),
+    appRoot: path.resolve(__dirname, '../'),
+    version: package.version,
+    name: package.name,
+    packageDestination: path.resolve(__dirname, '../releases'),
+    ignoredPaths: (() => {
+      const paths = [
+        path.resolve(__dirname, '../src/'),
+        path.resolve(__dirname, '../index.html'),
+        path.resolve(__dirname, '../static/'),
+        path.resolve(__dirname, '../test/'),
+        path.resolve(__dirname, '../.cache/'),
+        path.resolve(__dirname, '../test/'),
+        path.resolve(__dirname, '../README.md'),
+        path.resolve(__dirname, '../releases/'),
+        path.resolve(__dirname, '../.babelrc'),
+        path.resolve(__dirname, '../.editorconfig'),
+        path.resolve(__dirname, '../.gitignore'),
+        path.resolve(__dirname, '../build/'),
+      ]
+
+      const regexps = paths.map((path) => {
+        return new RegExp(path)
+      })
+      return regexps
+    })(),
     {{else}}
     assetsPublicPath: '/',
     {{/if}}
